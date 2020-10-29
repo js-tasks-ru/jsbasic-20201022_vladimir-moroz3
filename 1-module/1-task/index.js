992be95595acdd299c0addd3976481755cc8748d
Vladimur <frostwowa@gmail.com>
2020-10-29 10:53:39 +0200

    1 module / 1 task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,11 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
-}
+  if (n === 0) return 1;
+  let res = n;  
+  for (var i = n; i > 1; i--) {
+    res *= (i - 1);
+  }
+   return res;
+ }
+
