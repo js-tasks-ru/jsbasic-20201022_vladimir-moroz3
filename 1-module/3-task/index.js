68a9cc779d34eea014783594aec02ab0f71f38d6
Vladimur <frostwowa@gmail.com>
2020-11-13 21:25:41 +0200

    1module/3task

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -4,5 +4,20 @@
  * @returns {string}
  */
 function ucFirst(str) {
-  // ваш код...
+  let strSmall = str.toLowerCase(),
+      target = 'в',
+      targetSmall = target.toLowerCase(),
+      counter = 0;
+
+  if (!str) return str;
+
+  for (let char of strSmall) {
+    if (char.indexOf(targetSmall) != -1) {
+      counter++;
+      console.log(counter);
+    }
+  }
+  if (counter == 1) {
+    return (str[0].toUpperCase() + str.slice(1));
+  }
 }
